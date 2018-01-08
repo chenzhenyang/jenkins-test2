5b340fd6bee18d25c4ded74f49c56eb9b5c1798d
czy_1@qq.com <czy_1@qq.com>
2018-01-08 11:25:19 +0800

    1. test

---
+++ src/main/java/com/example/demo/JenkinsTest2Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTest2Application.class, args);
+	}
+}
